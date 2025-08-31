9f90dece03939c0bc66adb0af1650aa351842658
Mark Nady <92520896+BIKO22@users.noreply.github.com>
2025-09-01 00:48:21 +0300

    Update App.jsx task 1

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,11 @@
 
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
+    <div>
+      <WelcomeMessage />
+    </div>
   );
 }
 
