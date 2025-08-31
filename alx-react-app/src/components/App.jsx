27a19ff491ffe49aa20cfc7798e65ab863b81a34
BIKO22 <markdev2021@gmail.com>
2025-09-01 00:08:52 +0300

    adding new feature

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,4 +1,54 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
+const UserProfile = (props) => {
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <p>Age: {props.age}</p>
+            <p>Bio: {props.bio}</p>
+        </div>
+    );
+};
+
+export default UserProfile;
+
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <UserProfile 
+        name="Alice" 
+        age={25} 
+        bio="Loves hiking and photography" 
+      />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
 
 function App() {
   return (
